6b3e13fe41ab8520f9d7dc7a84dd140784d6c11c
Ksant <mkonstn@gmail.com>
2020-12-16 01:35:34 +0300

    Task1

---
+++ Tasks/Tasks1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tasks1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пожалуйста свое имя.");
+            String name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToLongDateString()}");
+            Console.ReadLine();
+        }
+    }
+}
