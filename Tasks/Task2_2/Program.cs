25fbecdd19f7705f4244a7047b72658cdbbb5ecb
Ksant <mkonstn@gmail.com>
2020-12-16 01:40:38 +0300

    Task2

---
+++ Tasks/Task2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пожалуйста введите номер текущего месяца");
+            int numOfCurrMonth = Convert.ToInt32(Console.ReadLine());
+            DateTime workDate = new DateTime(2020, numOfCurrMonth, 1);
+            Console.WriteLine($"Текущий месяц - {workDate.ToString("MMMM")}");
+        }
+    }
+}
---
+++ Tasks/Task2_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задайте номер месяца");
+            int numOfMonth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Задайте минимальную температуру");
+            double minT = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Задайте максимальную температуру");
+            double maxT = Convert.ToDouble(Console.ReadLine());
+            double averT = (minT + maxT) / 2;
+            bool winter = false;
+            switch (numOfMonth)
+            {
+                case 1:
+                    winter = true;
+                    break;
+                case 2:
+                    winter = true;
+                    break;
+                case 12:
+                    winter = true;
+                    break;
+            }
+            if (winter && averT > 0)
+            {
+                Console.WriteLine("Дождливая зима");
+            }
+        }
+    }
+}
