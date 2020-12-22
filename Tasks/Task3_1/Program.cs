72fa682e33ace8e8a3ea8a43477160af554efe5a
Ksant <mkonstn@gmail.com>
2020-12-22 23:18:08 +0300

    Task3

---
+++ Tasks/Task3_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Task3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            StringBuilder sb = new StringBuilder();
+  
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    sb.Append(" ");
+                    Console.WriteLine($"{sb.ToString()}{arr[i, j]}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tasks/Task3_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Task3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            for (int i = str.Length; i > 0; i--)
+            {
+                sb.Append(str[i-1]);
+            }
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
